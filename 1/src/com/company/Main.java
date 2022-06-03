94c4eacc5b0b9158e514e7f7e3288175b1f5055c
Ihor_lrt <99791477+ihorjfe@users.noreply.github.com>
2022-06-03 11:36:28 +0300

    Add files via upload

---
+++ 1/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int a = 2;a<=1000;a=2*a-1 ){
+            if (a>=200){
+                System.out.println(a);
+            }
+            }
+        }
+    }
+
